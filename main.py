bd9ac5caa28db40886416658d1f0e3f986af8d4d
Hanfeng Lin <nino.lin@163.com>
2023-02-21 23:33:38 -0600

    break if unexpected resn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-
 df = pd.read_csv("test.csv")
 cys_id = df.iloc[:, 1].tolist()
 cys_dict = {}
@@ -23,10 +22,11 @@ for filename in os.listdir(pdb_dir):
 print(pdb_files)
 
 cys_pLDDT_dict = {}
-for key in cys_dict:
-    for pdb in pdb_files:
+for key in cys_dict:  # choose protein
+    break_flag = False
+    for pdb in pdb_files:  # open protein AF2 model
         if key in pdb:
-            cys_list = cys_dict[key]
+            cys_list = cys_dict[key]  # go through each cys in this protein
             with open(pdb, "r") as f:
                 for line in f:
                     if line.startswith("ATOM"):
@@ -34,7 +34,9 @@ for key in cys_dict:
                         if resi in cys_list:
                             if line[17:20] == "CYS":
                                 bfactor = float(line[60:66])
-                                cys_pLDDT_dict[key+"_C"+str(resi)] = bfactor
+                                cys_pLDDT_dict[key + "_C" + str(resi)] = bfactor
+                            else:
+                                break
 
 print(cys_pLDDT_dict)
 
